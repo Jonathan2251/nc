484f67ed115864ab3e05cdad29e78996d8a99ff6
cschen <jonathan.chen@kernon.us>
2020-08-12 16:57:50 +0800

    print value_info

--- code/print_onnx.py
+++ code/print_onnx.py
@@ -3,3 +3,4 @@ import onnx
 onnx_model = onnx.load('00179_RP.onnx')
 #onnx.save(onnx_model, 'model_mod.onnx')
 print('The model is:\n{}'.format(onnx_model))
+print('After shape inference, the shape info of Y is:\n{}'.format(onnx_model.graph.value_info))
